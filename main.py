6fc93a548d66f659c7b23d375cc25785c18e5db4
liuqitoday <liuqitoday@163.com>
2019-05-13 13:37:04 +0800

    main.py

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     while True:
         doCheckin()
         sleepSecs = 60 * 60 * 24 + random.randint(1, 500)
-        print('下次执行时间' + sleepSecs + '秒后')
+        print('下次执行时间' + str(sleepSecs) + '秒后')
         time.sleep(sleepSecs)
