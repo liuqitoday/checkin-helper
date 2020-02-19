c99e99661982fe55e380f9c4752fe4369a8e1653
gxitm <xiaoxiao6208@gmail.com>
2020-02-19 22:43:23 +0800

    计划添加签到结果微信推送

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import configparser
 import time
+import requests
 import random
 import smzdm
 import lenovoclub
@@ -13,15 +14,29 @@ def doCheckin():
     smzdmCookies = config_raw.get('SMZDM', 'cookies')
     print ("什么值得买")
     smzdm.checkin(smzdmCookies)
-    print('\n')
     # 联想社区
     lenovoclubCookies = config_raw.get('LENOVOCLUB','cookies')
     lenovoclubToken = config_raw.get('LENOVOCLUB','token')
+    lenovoclubId = config_raw.get('LENOVOCLUB','lenovoid')
     print ("联想社区")
-    lenovoclub.checkin(lenovoclubCookies,lenovoclubToken)
+    lenovoclub.checkin(lenovoclubCookies,lenovoclubToken,lenovoclubId)
     # V2EX
     # v2exCookies = config_raw.get('V2EX', 'cookies')
     # v2ex.checkin(v2exCookies)
 
+def pushWechat(desp):
+    config_cfg = 'config.cfg'
+    config_raw = configparser.RawConfigParser()
+    config_raw.read(config_cfg)
+    ssckey = config_raw.get('SEVERCHAN', 'SCKEY')
+    #print (desp)
+    send_url='https://sc.ftqq.com/' + ssckey + '.send'
+    params = {
+        'text': '签到提醒'+ time.strftime('%Y-%m-%d %H:%M:%S'),
+        'desp': desp
+    }
+    requests.post(send_url,params=params)
+
 if __name__ == "__main__":
-        doCheckin()
+    doCheckin()
+    #pushWechat(doCheckin())
