e55475f57667e2761dc36c85d290dca62b866c80
gxitm <xiaoxiao6208@gmail.com>
2020-02-19 22:41:15 +0800

    修改联想签到

--- lenovoclub.py
+++ lenovoclub.py
@@ -11,21 +11,34 @@ def is_json(myjson):
         return False
     return True
 
-def checkin(myCookie,myToken):
+def checkin(myCookie,myToken,myId):
+    callbackParam1 = 'jQuery183048434872539258844_' + str(int(round(time.time() * 1000)))
+    callbackParam2 = str(int(round(time.time() * 1000)))
+    #url_signlist = https://club.lenovo.com.cn/signlist
+    url_userinfo = 'https://i.lenovo.com.cn/mcenter/getUserNameAndUserLevel.jhtml?lenovoId=' + myId + '&sts=e40e7004-4c8a-4963-8564-31271a8337d8&callback=' + callbackParam1 + '&_=' + callbackParam2
     url_checkin = 'https://club.lenovo.com.cn/sign'
-    headers = {
-        'origin': 'https://club.lenovo.com.cn',
-        'Referer': 'https://club.lenovo.com.cn/signlist',
+    signlist = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'zh-CN,zh;q=0.9',
-        ':authority': 'club.lenovo.com.cn',
-        ':path': '/sign',
-        ':scheme': 'https',
         'cache-control': 'max-age=0',
-        'x-requested-with': 'XMLHttpRequest',
+        'cookie': myCookie,
+        'referer': 'https://club.lenovo.com.cn/thread-1814833-1-1.html',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+    }
+
+    headers = {
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'content-length': '47',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'cookie': myCookie,
+        'origin': 'https://club.lenovo.com.cn',
+        'referer': 'https://club.lenovo.com.cn/signlist',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        'Cookie': myCookie
+        'x-requested-with': 'XMLHttpRequest'
     }
 
     data = {
@@ -34,15 +47,23 @@ def checkin(myCookie,myToken):
     
     #headersdata=json.dumps(headers)  # 如遇特殊符号（:authority: club.lenovo.com.cn，:scheme: https） 需字典数据转为json，需要使用json.dumps
     session = requests.session()
-    response = session.post(url_checkin, json=headers, data=data, verify=True) #json=data 转为json解决headers特殊符号
-    if is_json(response.text) == "True":
-        #response.content.decode("unicode_escape") # 编码转换为汉字
-        result = json.loads(response.text)
-        if result['code'] == 100000 :
-            print('联想社区签到成功，累计获得延保' + str(result['msg'],result['data']['signCal']['user_yanbao_score']) + '天')
-        elif result['code'] == 100001 :
-            print('签到失败:' + str(result['msg']))
+    response_userinfo = session.get(url_userinfo,headers=signlist,verify=True)
+    result_userinfo = json.loads(response_userinfo.text.replace(callbackParam1, '')[1:-1])
+    response = session.post(url_checkin, headers=headers, data=data) #json=headers 转为json解决headers特殊符号':authority': 'club.lenovo.com.cn',':path': '/sign',':scheme': 'https',
+    result = response.content.decode("unicode_escape")# unicode编码转换为汉字
+    try:
+        result_checkin = json.loads(result)
+        if result_checkin['code'] == 100000 :
+            print('用户名:'+ str(result_userinfo['data']['username']))
+            print('用户id:'+ myId)
+            print('签到成功，本次获得' + str(result_checkin['data']['data']['add_yb_tip']))
+            print('已持续签到' + str(result_checkin['data']['signCal']['continue_count']) + '天')
+            print('已累计获得延保' + str(result_checkin['data']['signCal']['user_yanbao_score']) + '天')
+        elif result_checkin['code'] == 100001 :
+            print('用户名:'+ str(result_userinfo['data']['username']))
+            print('用户id:'+ myId)
+            print(str(result_checkin['msg']))
         else:
-            print('签到失败:' + str(result['msg']))
-    else:
-        print('签到失败:非Json格式')
+            print ('签到失败')
+    except ValueError:
+        print ('网页打开失败，非json')
